d36e6ba6211d487c0decc902b0f919894cd229a6
Amanda Gois <amandabalcacar@gmail.com>
2024-09-21 16:04:21 -0400

    feat: metodo put

--- server.js
+++ server.js
@@ -27,6 +27,33 @@ app.get('/users', async (req, res) => {
    res.status(200).json(users)
 })
 
+app.put('/users/:id', async (req, res) => {
+    
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data:{
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age
+        }
+    })
+})
+
+app.delete('/users/:id', async (req, res) => {
+   
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        },
+    data:{
+        name: req.body.name,
+        email: req.body.email,
+        age: req.body.age
+    }
+})
+})
 
 app.listen(3000)
 
