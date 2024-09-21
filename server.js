5904075b4a4088c58534ffac54b35f9b81126a70
Amanda Gois <amandabalcacar@gmail.com>
2024-09-21 15:45:50 -0400

    feat: conexão com o banco de dados

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
 
+
+const prisma = new PrismaClient()
 const app = express()
 app.use(express.json())
 
-const user = []
+app.post('/users', async (req, res) => {
+    
+    await prisma.user.create({
+        data: {
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age
+        }
+    })
 
-app.post('/users', (req, res) => {
-    
-    user.push(req.body)
-    
-    res.send('Ok, post')
+    res.status(201).json(req.body)
 })
 
 
-app.get('/users', (req, res) => {
-    res.json(user)
+app.get('/users', async (req, res) => {
+
+    const users = await prisma.user.findMany()
+
+   res.status(200).json(users)
 })
 
 
